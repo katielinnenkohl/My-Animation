e2d72a72deca4def6630fc9a89b5b9e2979956f6
Katherine R Linnenkohl <kl88081@srcs.loc>
2018-05-30 11:00:06 -0700

    palp fixed

--- Animate.js
+++ Animate.js
@@ -1,14 +1,25 @@
 var palpatine = document.getElementById("animation4");
-var xpos = 440;
-var ypos = 350;
-var id= setInterval(frame, 5);
+var ypos1 = 440;
+var saber = document.getElementById("animation5");
+var xpos2 = 980;
+var id = setInterval(frame, 7);
+
+
 
 function frame(){
-    if( ypos == 0){
+    if( ypos1 == 110){
+        clearInterval(id);
+    } else{
+        ypos1--;
+        animation4.style.top = ypos1 + 'px';
+    }
+    if( xpos2 == 370){
         clearInterval(id);
     }
     else{
-        ypos--;
-        animation4.style.top = ypos + 'px';
+        xpos2++;
+        animation5.style.left = xpos2 + 'px';
     }
+
 }
+
